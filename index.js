9e66aac637f5d35196e067e71ed150f0faa067fe
AmSmo <adam.smolenski@gmail.com>
2020-09-12 15:39:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards (people, wishes){
+    let answer = [];
+    for (let i=0; i<people.length; i++){
+        answer.push(`Thank you, ${people[i]}, for the wonderful ${wishes} gift!`);
+    }
+    return answer;
+}
+
+function countDown(num){
+    let i = num;
+    while (i>=0){
+        console.log(i);
+        i--;
+    }
+
+}
